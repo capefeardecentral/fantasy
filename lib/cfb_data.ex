5997ef72c2a5bacb4a16d1a6216fb3ea2ae61e4b
Cape Fear Decentral <info@capefeardecentral.net>
2023-10-02 20:32:27 -0400

    add helpers

--- lib/cfb_data.ex
+++ lib/cfb_data.ex
@@ -18,7 +18,8 @@ defmodule Fantasy.CFBData do
       "team" => team
     }
 
-    make_request(endpoint, params)
+    {_, stats} = make_request(endpoint, params)
+    Poison.decode(stats.body)
   end
 
   def get_game_team_stats(team, year) do
@@ -30,6 +31,25 @@ defmodule Fantasy.CFBData do
     }
 
     make_request(endpoint, params)
+    {_, stats} = make_request(endpoint, params)
+    Poison.decode(stats.body)
+  end
+
+  def compile_season_defense_stats(games, team) do
+    # get opponents from each game
+    opponents =
+      Enum.map(games, fn game ->
+        Enum.find(game["teams"], fn team -> team.school != team end)
+      end)
+
+    stats = %{
+      "rushing_tds" => 0,
+      "rushing_yds" => 0,
+      "passing_tds" => 0,
+      "passing_yds" => 0
+    }
+
+    # enum reduce to compile
   end
 
   def make_request(endpoint, params \\ %{}) do
